52c881fc4c129f1cb1a1f46821f0531e12104e0c
Rob Stradling <rob@sectigo.com>
2019-04-15 20:19:47 +0100

    Change github path.

--- plgo/modulewriter.go
+++ plgo/modulewriter.go
@@ -105,7 +105,7 @@ func readPlGoSource() ([]byte, error) {
 		goPath = build.Default.GOPATH // Go 1.8 and later have a default GOPATH
 	}
 	for _, goPathElement := range filepath.SplitList(goPath) {
-		rv, err := ioutil.ReadFile(filepath.Join(goPathElement, "src", "github.com", "microo8", "plgo", "pl.go"))
+		rv, err := ioutil.ReadFile(filepath.Join(goPathElement, "src", "github.com", "robstradling", "plgo", "pl.go"))
 		if err == nil {
 			return rv, nil
 		} else if os.IsNotExist(err) {
@@ -114,7 +114,7 @@ func readPlGoSource() ([]byte, error) {
 			return nil, fmt.Errorf("Cannot read plgo package: %s", err)
 		}
 	}
-	return nil, fmt.Errorf("Package github.com/microo8/plgo not installed\nplease install it with: go get -u github.com/microo8/plgo/plgo")
+	return nil, fmt.Errorf("Package github.com/robstradling/plgo not installed\nplease install it with: go get -u github.com/robstradling/plgo/plgo")
 }
 
 func (mw *ModuleWriter) writeplgo(tempPackagePath string) error {
